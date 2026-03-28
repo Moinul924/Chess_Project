50c24b03c21c764c56dc1f9c82cc9288bdf825cf
moinul <ripon.moinull@student.manchester.ac.uk>
2026-03-28 22:54:29 +0000

    first commit

---
+++ src/test/java/com/chess/Chess_Project/ChessProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chess.Chess_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
